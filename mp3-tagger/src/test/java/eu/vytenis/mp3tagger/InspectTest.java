578dbd8a60b9bd581169f409dc454b2a4de7e551
Vytenis Bivainis <monrealis@gmail.com>
2018-12-27 17:16:40 +0200

    Inspect tags

---
+++ mp3-tagger/src/test/java/eu/vytenis/mp3tagger/InspectTest.java
@@ -0,0 +1,31 @@
+package eu.vytenis.mp3tagger;
+
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
+
+class InspectTest {
+    String path = null;
+
+    @Test
+    void test() throws Exception {
+        assumeTrue(path != null);
+
+        Mp3File file = new Mp3File(path);
+        System.out.println(file.hasId3v1Tag());
+        System.out.println(file.hasId3v2Tag());
+        System.out.println(file.hasCustomTag());
+        ID3v1 v1 = file.getId3v1Tag();
+        ID3v2 v2 = file.getId3v2Tag();
+
+        System.out.println(v1.getTrack());
+        System.out.println(v2.getTrack());
+        System.out.println(v2.getPartOfSet());
+        System.out.println(v1.getTitle());
+        System.out.println(v2.getTitle());
+    }
+}
